b41bb0161ba91369c2383df854126f216c148240
punishthecode <phanishmurthy2001@gmail.com>
2023-07-12 16:40:48 +0530

    Crud operations Done

--- kanbanboard/apis/urls.py
+++ kanbanboard/apis/urls.py
@@ -2,6 +2,15 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('homePage/', GetBoard.as_view()),
-    path('boardQuery/<str:board_id>/', GetBoardId.as_view())
+    path('boards/', GetBoard.as_view()),
+    path('postboard/', PostBoard.as_view()),
+    path('boardsbyid/<str:input_board_id>/', GetBoardId.as_view()),
+    path('deleteboard/<str:delete_board_id>', DeleteBoard.as_view()),
+    path('updateboard/<str:input_board_id>/', PutBoard.as_view()),
+
+    path('users/', GetUsers.as_view()),
+    path('postuser/', PostUsers.as_view()),
+    path('updateuser/<str:input_user_id>/', UpdateUsers.as_view()),
+    path('deleteuser/<str:delete_user_id>', DeleteUsers.as_view()),
+
 ]
--- kanbanboard/apis/views.py
+++ kanbanboard/apis/views.py
@@ -1,20 +1,77 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from django.views.generic import View
-from .serializers import BoardSerializer
+from .serializers import *
 from django.http import JsonResponse
-from .models import Board
-
+from .models import *
+from rest_framework import status
+from rest_framework.views import APIView
 # Create your views here.
 
-class GetBoard(View):
+#Board
+class GetBoard(APIView):
     def get(self, request):
         boards = Board.objects.all()
         serialized = BoardSerializer(boards, many=True)
-        return JsonResponse(serialized.data, safe=False)
+        return JsonResponse(serialized.data, status = status.HTTP_200_OK, safe=False)
     
-class GetBoardId(View):
-    def get(self, request, board_id):
-        board = Board.objects.filter(board_id=board_id)
-        serialized = BoardSerializer(board, data = request.data)
-        return JsonResponse(serialized.data, safe=False)
+class PostBoard(APIView):
+    def post(self, request):
+        serializer = BoardSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors)
+
+class PutBoard(APIView):
+    def put(self, request, input_board_id):
+        board = Board.objects.get(board_id = input_board_id)
+        serializer = BoardSerializer(board, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors)
+    
+class DeleteBoard(APIView):
+    def delete(self, request, delete_board_id):
+        board = Board.objects.get(board_id = delete_board_id)
+        board.delete()
+        return JsonResponse("Deleted Board", safe=False)
+    
+class GetBoardId(APIView):
+    def get(self, request, input_board_id):
+        board = Board.objects.get(board_id = input_board_id)
+        serialized = BoardSerializer(board, many = True)
+        return JsonResponse(serialized.data, status = status.HTTP_200_OK, safe=False)
+    
+#User
+class GetUsers(APIView):
+    def get(self, request):
+        users = User.objects.all()
+        serialized = UserSerializer(users, many=True)
+        return JsonResponse(serialized.data, status = status.HTTP_200_OK, safe=False)
+    
+class PostUsers(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors)
+    
+class UpdateUsers(APIView):
+    def put(self, request, input_user_id):
+        user = User.objects.get(user_id = input_user_id)
+        serializer = UserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors)
+    
+class DeleteUsers(APIView):
+    def delete(self, request, delete_user_id):
+        user = User.objects.get(user_id = delete_user_id)
+        user.delete()
+        return JsonResponse("Deleted User", safe=False)
+    
+#
